5550d0c98e6d0a9f4f4dd25f7bf47bd10ea60304
Adam Hemeon <w0434257@nscc.ca>
2021-01-30 13:11:14 -0400

    Adds project header java comment with student names.

--- SCRAPBook/app/src/main/java/nscc/capstone/scrapbook/MainActivity.java
+++ SCRAPBook/app/src/main/java/nscc/capstone/scrapbook/MainActivity.java
@@ -1,5 +1,18 @@
 package nscc.capstone.scrapbook;
 
+/* Project SCRAPBook:
+ * Nova Scotia Community College (2021) student project for INFT-3000 Capstone course.
+ *
+ * MIT License Copyright 2021
+ *
+ * Contributors:
+ * Adam Hemeon
+ * Brady Getson
+ * Craysyn Trottier
+ * Jayden Morehouse
+ * Kieran London
+ *
+ * */
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,4 +24,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
