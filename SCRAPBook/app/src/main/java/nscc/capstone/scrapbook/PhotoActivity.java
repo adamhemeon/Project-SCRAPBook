c00151075707eb52384112e4253841843aaf97a8
jay457 <W0427457@nscc.ca>
2021-03-24 10:39:43 -0300

    fixed over-capacity photo bug

--- SCRAPBook/app/src/main/java/nscc/capstone/scrapbook/PhotoActivity.java
+++ SCRAPBook/app/src/main/java/nscc/capstone/scrapbook/PhotoActivity.java
@@ -223,7 +223,6 @@ public class PhotoActivity extends AppCompatActivity {
                         CAMERA_CODE);
 
                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                // TODO: Camera functionality
                 try {
                     startActivityForResult(i, CAMERA_CODE);
                 } catch (ActivityNotFoundException e) {
@@ -249,34 +248,39 @@ public class PhotoActivity extends AppCompatActivity {
 
             int cdSize = cd.getItemCount();
 
-            for (int i = 0; i < cdSize; i++) {
-                if (result == Activity.RESULT_OK) {
-                    ClipData.Item item = cd.getItemAt(i);
-                    Uri targetUri = item.getUri();
-                    try {
-                        // TODO: get width from somewhere else
-                        bitmap = Bitmap.createScaledBitmap(BitmapFactory.
-                                        decodeStream(getContentResolver().openInputStream(targetUri)),
-                                imageViewPhoto1.getWidth(),
-                                imageViewPhoto1.getHeight(), true);
+            if (photoCount + cdSize <= 9) {
 
-                        bitmapList.add(bitmap);
-//                        if(bitmapList.size() == 9) {
-//                            break;
-//                        }
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
+                for (int i = 0; i < cdSize; i++) {
+                    if (result == Activity.RESULT_OK) {
+                        ClipData.Item item = cd.getItemAt(i);
+                        Uri targetUri = item.getUri();
+                        try {
+                            bitmap = Bitmap.createScaledBitmap(BitmapFactory.
+                                            decodeStream(getContentResolver().openInputStream(targetUri)),
+                                    imageViewPhoto1.getWidth(),
+                                    imageViewPhoto1.getHeight(), true);
+
+                            bitmapList.add(bitmap);
+
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
+            } else {
+                maxPhotoWarning();
             }
         } else if (requestCode == CAMERA_CODE) {
-            Bundle extras = intent.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            bitmap = Bitmap.createScaledBitmap(imageBitmap,
-                    imageViewPhoto1.getWidth(),
-                    imageViewPhoto1.getHeight(), true);
-            bitmapList.add(bitmap);
-
+            if(photoCount + 1 <= 9) {
+                Bundle extras = intent.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                bitmap = Bitmap.createScaledBitmap(imageBitmap,
+                        imageViewPhoto1.getWidth(),
+                        imageViewPhoto1.getHeight(), true);
+                bitmapList.add(bitmap);
+            } else {
+                maxPhotoWarning();
+            }
         }
 
         photoCount = bitmapList.size();
@@ -289,16 +293,23 @@ public class PhotoActivity extends AppCompatActivity {
                 btnStart.setEnabled(true);
             }
             textViewNumPhotos.setText(photoCount + "/9");
-        }else{
-            btnStart.setEnabled(false);
-            Context context = getApplicationContext();
-            CharSequence text = "Too many images selected!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-
         }
+
+    }
+
+    protected void maxPhotoWarning(){
+        photoCount = bitmapList.size();
+
+        if(photoCount != 9) {
+            btnStart.setEnabled(false);
+        }
+
+        Context context = getApplicationContext();
+        CharSequence text = "Too many images selected!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     protected void setImages(){
